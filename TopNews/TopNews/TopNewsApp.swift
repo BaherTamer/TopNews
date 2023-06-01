5380002598fdbd95cfe0eaa30aae4ee88b0677e2
BaherTamerHashem <baher.appledev@gmail.com>
2023-06-01 14:26:42 +0300

    - Set up Xcode files

---
+++ TopNews/TopNews/TopNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopNewsApp.swift
+//  TopNews
+//
+//  Created by Baher Tamer on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TopNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
